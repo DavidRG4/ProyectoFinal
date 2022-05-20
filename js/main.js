2a5d89af17cc8715e8a16fa03f31440d947616a9
DavidRG4 <alum.djesusriverog.2020@iesalixar.org>
2022-05-20 16:19:05 +0200

    version-text_1.1.0

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ let READY_STATE_COMPLETE = 4;
 let HTTP_STATUS_OK = 200;
 
 window.onload = () => {
+  showAllContainers();
   document
     .getElementById("AllContainers")
     .addEventListener("click", showAllContainers);
@@ -13,9 +14,11 @@ window.onload = () => {
     .getElementById("searchContainer")
     .addEventListener("click", inspecContainer);
 };
+//Login
 
+
+//General
 function showAllContainers() {
-  document.getElementById("result").innerHTML = "hola";
   let xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () {
     if (
@@ -40,13 +43,16 @@ function showAllImages() {
       procesar_imagenes(data);
     }
   };
-  xhr.open("GET", "http://172.17.0.1:2327/images/json");
+  xhr.open("GET", "http://127.0.0.1:2327/images/json");
   xhr.send();
 }
 function procesar_container(containers) {
   document.getElementById("result").innerHTML = "";
   let table = document.createElement("table");
+  table.setAttribute("class", "table table-striped table-hover");
   let tbody = document.createElement("tbody");
+  let thead = document.createElement("thead");
+  thead.setAttribute("class", "table-primary");
   let tr = document.createElement("tr");
   let th = document.createElement("th");
   th.innerHTML = "Nombre";
@@ -63,7 +69,8 @@ function procesar_container(containers) {
   th = document.createElement("th");
   th.innerHTML = "Interacciones";
   tr.appendChild(th);
-  tbody.appendChild(tr);
+  thead.appendChild(tr);
+
   for (let container of containers) {
     tr = document.createElement("tr");
     let td = document.createElement("td");
@@ -75,36 +82,57 @@ function procesar_container(containers) {
     td = document.createElement("td");
     td.innerHTML = container.State;
     tr.appendChild(td);
+
     td = document.createElement("td");
-    let puerto = container.Ports[0].PublicPort;
-    td.innerHTML = "Port: " + puerto;
-    tr.appendChild(td);
+    if (container.Ports.length > 0) {
+      let puerto = container.Ports[0].PublicPort;
+      td.innerHTML = "Port: " + puerto;
+      tr.appendChild(td);
+    } else {
+      td.innerHTML = "Port: 8080";
+      tr.appendChild(td);
+    }
     td = document.createElement("td");
+    let spanStart = document.createElement("span");
     let start = document.createElement("i");
     start.setAttribute("class", "fa-solid fa-play");
-    start.setAttribute("value", container.Id);
+    start.setAttribute("id", container.Id);
+    spanStart.appendChild(start);
+    spanStart.setAttribute("class", "start");
+    let spanStop = document.createElement("span");
     let stop = document.createElement("i");
     stop.setAttribute("class", "fa-solid fa-circle-stop");
-    stop.setAttribute("value", container.Id);
-    stop.setAttribute("id");
+    stop.setAttribute("id", container.Id);
+    spanStop.appendChild(stop);
+    spanStop.setAttribute("class", "stop");
+    let spanEliminate = document.createElement("span");
     let eliminate = document.createElement("i");
     eliminate.setAttribute("class", "fa-solid fa-trash-arrow-up");
-    eliminate.setAttribute("value", container.Id);
+    eliminate.setAttribute("id", container.Id);
+    spanEliminate.appendChild(eliminate);
+    spanEliminate.setAttribute("class", "eliminate");
 
-    td.appendChild(start);
-    td.appendChild(stop);
-    td.appendChild(eliminate);
+    td.appendChild(spanStart);
+    td.appendChild(spanStop);
+    td.appendChild(spanEliminate);
     tr.appendChild(td);
 
     tbody.appendChild(tr);
+    spanStart.addEventListener("click", interact);
+    spanStop.addEventListener("click", interact);
+    spanEliminate.addEventListener("click", interact);
   }
+  table.appendChild(thead);
   table.appendChild(tbody);
   document.getElementById("result").appendChild(table);
 }
 function procesar_imagenes(imagenes) {
   document.getElementById("result").innerHTML = "";
   let table = document.createElement("table");
+  table.setAttribute("class", "table table-striped table-hover");
   let tbody = document.createElement("tbody");
+  let thead = document.createElement("thead");
+  thead.setAttribute("class", "table-primary");
   let tr = document.createElement("tr");
   let th = document.createElement("th");
   th.innerHTML = "Nombre";
@@ -112,7 +140,10 @@ function procesar_imagenes(imagenes) {
   th = document.createElement("th");
   th.innerHTML = "Id";
   tr.appendChild(th);
-  tbody.appendChild(tr);
+  th = document.createElement("th");
+  th.innerHTML = "Interacciones";
+  tr.appendChild(th);
+  thead.appendChild(tr);
   for (let imagen of imagenes) {
     tr = document.createElement("tr");
     let td = document.createElement("td");
@@ -121,14 +152,24 @@ function procesar_imagenes(imagenes) {
     td = document.createElement("td");
     td.innerHTML = imagen.Id;
     tr.appendChild(td);
+    td = document.createElement("td");
+    let spanEliminate = document.createElement("span");
+    let eliminate = document.createElement("i");
+    eliminate.setAttribute("class", "fa-solid fa-trash-arrow-up");
+    eliminate.setAttribute("id", imagen.Id);
+    spanEliminate.appendChild(eliminate);
+    spanEliminate.setAttribute("class", "eliminate");
+    td.appendChild(spanEliminate)
+    tr.appendChild(td);
     tbody.appendChild(tr);
   }
+  table.appendChild(thead);
   table.appendChild(tbody);
   document.getElementById("result").appendChild(table);
 }
 
+//Especificos
 function InspectImagen() {
-  document.getElementById("result").innerHTML = "hola";
   let xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () {
     if (
@@ -147,7 +188,10 @@ function InspectImagen() {
 function procesar_imagen(imagen) {
   document.getElementById("result").innerHTML = "";
   let table = document.createElement("table");
+  table.setAttribute("class", "table table-stripped table-hover");
   let tbody = document.createElement("tbody");
+  let thead = document.createElement("thead");
+  thead.setAttribute("class", "table-primary");
   let tr = document.createElement("tr");
   let th = document.createElement("th");
   th.innerHTML = "Nombre";
@@ -155,7 +199,10 @@ function procesar_imagen(imagen) {
   th = document.createElement("th");
   th.innerHTML = "Id";
   tr.appendChild(th);
-  tbody.appendChild(tr);
+  th = document.createElement("th");
+  th.innerHTML = "Interacciones";
+  tr.appendChild(th);
+  thead.appendChild(tr);
   tr = document.createElement("tr");
   let td = document.createElement("td");
   td.innerHTML = imagen.RepoTags[0];
@@ -163,13 +210,22 @@ function procesar_imagen(imagen) {
   td = document.createElement("td");
   td.innerHTML = imagen.Id;
   tr.appendChild(td);
+  td = document.createElement("td");
+  let spanEliminate = document.createElement("span");
+  let eliminate = document.createElement("i");
+  eliminate.setAttribute("class", "fa-solid fa-trash-arrow-up");
+  eliminate.setAttribute("id", imagen.Id);
+  spanEliminate.appendChild(eliminate);
+  spanEliminate.setAttribute("class", "eliminate");
+  td.appendChild(spanEliminate)
+  tr.appendChild(td);
+  table.appendChild(thead);
   tbody.appendChild(tr);
   table.appendChild(tbody);
   document.getElementById("result").appendChild(table);
 }
 
 function inspecContainer() {
-  document.getElementById("result").innerHTML = "hola";
   let xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () {
     if (
@@ -188,7 +244,10 @@ function inspecContainer() {
 function procesar_contenedor(container) {
   document.getElementById("result").innerHTML = "";
   let table = document.createElement("table");
+  table.setAttribute("class", "table table-stripped table-hover");
   let tbody = document.createElement("tbody");
+  let thead = document.createElement("thead");
+  thead.setAttribute("class", "table-primary");
   let tr = document.createElement("tr");
   let th = document.createElement("th");
   th.innerHTML = "Nombre";
@@ -205,39 +264,125 @@ function procesar_contenedor(container) {
   th = document.createElement("th");
   th.innerHTML = "Interacciones";
   tr.appendChild(th);
-  tbody.appendChild(tr);
+  thead.appendChild(tr);
   tr = document.createElement("tr");
   let td = document.createElement("td");
-  td.innerHTML = container.Names[0];
+  td.innerHTML = container.Name;
   tr.appendChild(td);
   td = document.createElement("td");
-  td.innerHTML = container.Image;
+  td.innerHTML = container.Config.Image;
   tr.appendChild(td);
   td = document.createElement("td");
-  td.innerHTML = container.State;
+  td.innerHTML = container.State.Status;
   tr.appendChild(td);
   td = document.createElement("td");
-  let puerto = container.Ports[0].PublicPort;
-  td.innerHTML = "Port: " + puerto;
-  tr.appendChild(td);
+  //console.log(container.NetworkSettings.Ports)
+  //let puerto = container.Ports
+  //td.innerHTML = "Port: " + puerto;
+  //tr.appendChild(td);
   td = document.createElement("td");
+  let spanStart = document.createElement("span");
   let start = document.createElement("i");
   start.setAttribute("class", "fa-solid fa-play");
-  start.setAttribute("value", container.Id);
+  start.setAttribute("id", container.Id);
+  spanStart.appendChild(start);
+  spanStart.setAttribute("class", "start");
+  let spanStop = document.createElement("span");
   let stop = document.createElement("i");
   stop.setAttribute("class", "fa-solid fa-circle-stop");
-  stop.setAttribute("value", container.Id);
-  stop.setAttribute("id");
+  stop.setAttribute("id", container.Id);
+  spanStop.appendChild(stop);
+  spanStop.setAttribute("class", "stop");
+  let spanEliminate = document.createElement("span");
   let eliminate = document.createElement("i");
   eliminate.setAttribute("class", "fa-solid fa-trash-arrow-up");
-  eliminate.setAttribute("value", container.Id);
+  eliminate.setAttribute("id", container.Id);
+  spanEliminate.appendChild(eliminate);
+  spanEliminate.setAttribute("class", "eliminate");
 
-  td.appendChild(start);
-  td.appendChild(stop);
-  td.appendChild(eliminate);
+  td.appendChild(spanStart);
+  td.appendChild(spanStop);
+  td.appendChild(spanEliminate);
   tr.appendChild(td);
 
   tbody.appendChild(tr);
+  spanStart.addEventListener("click", interact);
+  spanStop.addEventListener("click", interact);
+  spanEliminate.addEventListener("click", interact);
+  tr.appendChild(td);
+
+  tbody.appendChild(tr);
+  table.appendChild(thead);
   table.appendChild(tbody);
   document.getElementById("result").appendChild(table);
+}
+
+//Interacciones
+function interact(action) {
+  id = action.target.id;
+  interactType = action.currentTarget.className;
+  console.log(interactType);
+  if (interactType === "start") {
+    startContainer(id);
+  } else if (interactType === "stop") {
+    stopContainer(id);
+  } else {
+    deleteContainer(id);
+  }
+}
+//Eliminar
+function deleteContainer(id) {
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (
+      xhr.readyState === READY_STATE_COMPLETE &&
+      xhr.status === HTTP_STATUS_OK
+    ) {
+    }
+  };
+  url = "http://127.0.0.1:2327/containers/" + id;
+  xhr.open("Delete", url);
+  xhr.send();
+  showAllContainers();
+}
+
+//Iniciar
+function startContainer(id) {
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (
+      xhr.readyState === READY_STATE_COMPLETE &&
+      xhr.status === HTTP_STATUS_OK
+    ) {
+    }
+  };
+  url = "http://127.0.0.1:2327/containers/" + id + "/start";
+  xhr.open("Post", url);
+  xhr.send();
+  
+  showAllContainers();
+}
+
+//Parar
+function stopContainer(id) {
+  let xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function () {
+    if (
+      xhr.readyState === READY_STATE_COMPLETE &&
+      xhr.status === HTTP_STATUS_OK
+    ) {
+    }
+  };
+  url = "http://127.0.0.1:2327/containers/" + id + "/stop";
+  xhr.open("Post", url);
+  xhr.send();
+  showAllContainers();
+
+}
+
+//Crear Contenedor
+function createForm() {
+  creator = document.getElementById("creator");
+  form = document.createElement("form");
 }
